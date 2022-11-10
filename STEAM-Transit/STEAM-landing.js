e37c773b2ec6839d9e7844ee19c52386ad230aa0
Mike Odnis <airwolf635@gmail.com>
2022-11-10 10:19:11 -0500

    Lets see where this goes

---
+++ STEAM-Transit/STEAM-landing.js
@@ -0,0 +1,36 @@
+import React from "React";
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow,
+}   from "@react-google-maps/api";
+import {formatRelative } from "data-fns";
+// import usePlacesAutocomplete, {
+//    getGeocode,
+//    getLating,
+//  } from "use-places-autocomplete";
+//  impoort {
+//      Combobox,    
+//      ComboboxInput,
+//      ComboboxPopover,
+//      ComboboxList
+//      ComboboxOption,
+//  }   from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+export default function App() {
+    const {} useLoadScript({})
+        googleMapApiKey:
+    })
+    return <div>Map</div>;
+
+function initMap(){
+    //Map
+    var options = {
+        center: {},
+        zoom
+    }
+
+    map = new google.maps.Map(document.getElementById)
+}
