5d64da2d44a2085c9befe452d520bb63be7498e9
Mike Odnis <airwolf635@gmail.com>
2022-11-11 15:19:14 -0500

    Fonts I guess

--- STEAM-Transit/STEAM-landing.js
+++ STEAM-Transit/STEAM-landing.js
@@ -19,11 +19,29 @@ import {formatRelative } from "data-fns";
 //  }   from "@reach/combobox";
 import "@reach/combobox/styles.css";
 
+const libraries = ["places"]
+const mapContainerStyle = {
+    width: "100vw",
+    height: "100vh",
+};
+const center = {
+    lat:40.712776;
+    log
+}
+
 export default function App() {
-    const {} useLoadScript({})
-        googleMapApiKey:
-    })
-    return <div>Map</div>;
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapApiKey: ProcessingInstruction.end.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries,
+    });
+
+    if(loadError) return "Error loading maps";
+    if(!isLoaded) return "Loading Maps";
+    
+    return <div>
+        <GoogleMap mapContainerStyle = {mapContainerStyle} zoom=8 ></GoogleMap>
+    </div>;
+}
 
 function initMap(){
     //Map
